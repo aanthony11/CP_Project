a649c54f450e90ad9a53bfa1e5fc937c8cc85eb7
Manish Rajendran <rajend16@msu.edu>
2020-03-25 19:36:06 -0400

    Initial Commit

---
+++ CP_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CP_Project
+//
+//  Created by Manish Rajendran on 3/25/20.
+//  Copyright © 2020 Manish Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
