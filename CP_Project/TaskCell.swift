e1f1b58ecf1c574df75667dcbc27ec7625726a6a
Adam Anderson <adamcanderson400@gmail.com>
2020-04-15 12:20:28 -0400

    Added task view controller.

---
+++ CP_Project/TaskCell.swift
@@ -0,0 +1,28 @@
+//
+//  TaskCell.swift
+//  CP_Project
+//
+//  Created by Adam Anderson on 4/15/20.
+//  Copyright © 2020 Manish Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var didCompleteImageVIew: UIImageView!
+    @IBOutlet weak var infoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CP_Project/TasksViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TasksViewController.swift
+//  CP_Project
+//
+//  Created by Adam Anderson on 4/15/20.
+//  Copyright © 2020 Manish Rajendran. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Get Tasks from Parse
+        let query = PFQuery(className: "Tasks")
+        query.includeKeys(["task"])
+        
+        query.findObjectsInBackground { (tasks, error) in
+            if tasks != nil {
+                self.tasks = tasks!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") as! TaskCell
+        
+//        cell.taskLabel.text = tasks["task"] as? String
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
