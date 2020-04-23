2710f8c451a64a55d0262b4b257fcec058583fd6
Manish Rajendran <rajend16@msu.edu>
2020-04-22 23:03:51 -0400

    completed group creation

--- CP_Project/CreateGroupViewController.swift
+++ CP_Project/CreateGroupViewController.swift
@@ -9,43 +9,104 @@
 import UIKit
 import Parse
 
-class CreateGroupViewController: UIViewController, UISearchBarDelegate {
-    
+class CreateGroupViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var groupName: UITextField!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    var usersTempArray:[PFUser] = []
+    var emailsTempArray:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        searchBar.autocapitalizationType = .none
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onCreate(_ sender: Any) {
+        if usersTempArray.count > 1 && groupName.text != "" {
+            let group = PFObject(className:"Group")
+            group["name"] = groupName.text
+            group["users"] = usersTempArray
+            group["count"] = usersTempArray.count
+            group.saveInBackground { (succeeded, error)  in
+                if (succeeded) {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(error?.localizedDescription)
+                }
+            }
+        } else {
+            print("Need at least one and/or group name")
+        }
+        
+
+
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(usersTempArray)
+        print(emailsTempArray)
+//        for object in usersTempArray {
+//              print(object.username!)
+//          }
+        
         let username = searchBar.text!.lowercased()
-        print(username)
         
-//        var currentUser = PFUser.current()
-//        print(currentUser?.username)
-        
-        let query = PFQuery(className:"User")
+        let query = PFQuery(className:"_User")
         query.whereKey("username", equalTo:username)
         query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
             if let error = error {
                 // Log details of the failure
                 print(error.localizedDescription)
             } else if let objects = objects {
-                print(objects)
                 // The find succeeded.
                 print("Successfully retrieved \(objects.count) users.")
                 // Do something with the found objects
                 for object in objects {
-                    print(object.objectId as Any)
+                    let tempUser = object as! PFUser
+                    let tempEmail = tempUser.username!
+                    if self.emailsTempArray.contains(tempEmail){
+                        print("User already in array")
+                    } else {
+                        self.usersTempArray.append(tempUser)
+                        self.emailsTempArray.append(tempUser.username!)
+                        self.searchBar.text = ""
+                    }
                 }
             }
-        }
-
+        self.tableView.reloadData()
+        } //end query
         
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailsTempArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = emailsTempArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection
+                                section: Int) -> String? {
+       return "Users to be added to group: "
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
     
 
     /*
